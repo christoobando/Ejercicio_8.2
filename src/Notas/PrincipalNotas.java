7de2581c1f2e8744c834b775be4ebb934345fec0
Christopher <Christopher@DESKTOP-6E1TUBO>
2024-09-03 15:06:02 -0500

    Actividad 5 - Ejercicio 8.2

---
+++ src/Notas/PrincipalNotas.java
@@ -0,0 +1,10 @@
+package Notas;
+
+public class PrincipalNotas {
+    public static void main(String[] args) {
+        VentanaPrincipal miVentanaPrincipal;
+        miVentanaPrincipal = new VentanaPrincipal();
+        miVentanaPrincipal.setVisible(true);
+    }
+}
+
